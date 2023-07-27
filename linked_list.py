f761c656c7ec2f726c49566e500217e3e58d05d2
Aditi Srivastav <srivastavaditi229@gmail.com>
2023-07-27 13:01:57 +0530

    change done

--- linked_list.py
+++ linked_list.py
@@ -1,35 +1,96 @@
 class Node:
-    def _init_(self, data):
+    def __init__(self, data):
         self.data = data
         self.next = None
 
+
 class LinkedList:
-    def _init_(self):
+    def __init__(self):
         self.head = None
-        
-    def append(self, data):
+
+    def insert_at_beginning(self, data):
         new_node = Node(data)
+
         if self.head is None:
+            self.head = new_node
+        else:
+            new_node.next = self.head
+            self.head = new_node
+
+    def insert_at_end(self, data):
+        new_node = Node(data)
+        if not self.head:
             self.head = new_node
             return
         last_node = self.head
         while last_node.next:
             last_node = last_node.next
         last_node.next = new_node
-        
+
+
+    def delete(self, key):
+        current_node = self.head
+        if current_node and current_node.data == key:
+            self.head = current_node.next
+            current_node = None
+            return
+        prev = None
+        while current_node:
+            if current_node.data == key:
+                break
+            prev = current_node
+            current_node = current_node.next
+        if current_node is None:
+            return
+        prev.next = current_node.next
+        current_node = None
+
+    def delete_at_beginning(self):
+        if self.head:
+            current_node = self.head
+            self.head = current_node.next
+            current_node = None
+
+    def search(self, key):
+        current_node = self.head
+        prev = None
+        index = 0
+        while current_node:
+            if current_node.data == key:
+                print("Found at index:", index)
+                return
+            prev = current_node
+            current_node = current_node.next
+            index += 1
+        print("Not found in the linked list")
+
     def print_list(self):
         current_node = self.head
         while current_node:
-            print(current_node.data)
+            print(current_node.data, end=" -> ")
             current_node = current_node.next
+        print("None")
 
-linked_list = LinkedList()
 
-while True:
-    element = input("Enter an element of the linked list (or enter 'q' to stop): ")
-    if element == 'q':
-        break
-    linked_list.append(int(element))
+# Example usage:
+if __name__ == "__main__":
+    linked_list = LinkedList()
+    linked_list.insert_at_end(1)
+    linked_list.insert_at_end(2)
+    linked_list.insert_at_end(3)
+    linked_list.insert_at_end(4)
+    linked_list.insert_at_end(5)
 
-linked_list.print_list()
+    linked_list.print_list()  # Output: 1 -> 2 -> 3 -> 4 -> 5 -> None
 
+    linked_list.insert_at_beginning(0)
+    linked_list.print_list()  # Output: 0 -> 1 -> 2 -> 3 -> 4 -> 5 -> None
+
+    linked_list.delete_at_beginning()
+    linked_list.print_list()  # Output: 1 -> 2 -> 3 -> 4 -> 5 -> None
+
+    linked_list.delete(3)
+    linked_list.print_list()  # Output: 1 -> 2 -> 4 -> 5 -> None
+
+    print(linked_list.search(3))  # Output: False
+    print(linked_list.search(4))  # Output: True
